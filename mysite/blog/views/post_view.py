a955a934707e6eb58134ac59b283146da13a06b3
Wash Amaral <washmrl.silva@gmail.com>
2025-03-07 07:06:39 -0300

    adding home view to django page

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -3,7 +3,7 @@ from django.views import generic
 
 class PostView(generic.DetailView):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello, world.")
+        return HttpResponse("Hello, world. This is my first django project.")
     # model = Post
     # template_name = 'blog/post.html'
     # context_object_name = 'post'
